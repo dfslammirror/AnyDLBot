e75da60f1891f2a0acb913c826d6b8554d2b4d93
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-06 09:59:14 -0600

    Initialize improving-download

---
+++ plugins/callback_handler.py
@@ -0,0 +1,15 @@
+import asyncio
+import os
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery
+#from plugins.youtube_dl_button import d_directory
+
+'''
+ads = d_directory()
+@Client.on_callback_query(filters.regex(r'^progress$'))
+async def ytdl_progress(bot, cb: CallbackQuery):
+    print('Pgreasc'*10)
+    await cb.answer(f"Downloaded : {ads}", True)
+'''   
+ 
+
